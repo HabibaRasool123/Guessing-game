a27b6d77570bcfa6443fc9f2b2d156eb78b0c224
HabibaRasool123 <165895338+HabibaRasool123@users.noreply.github.com>
2024-04-03 16:19:33 +0500

    Add files via upload

---
+++ code.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random()* 6+1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between (1 to 6):",
+    }
+
+]);
+
+if(answers.userGuessNumber === randomNumber)
+{
+console.log("congragulations! you guess right number.");
+}
+else{
+    console.log("you guess wrong number");
+}
